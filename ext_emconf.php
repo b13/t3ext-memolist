b7e5d2c95130da14306595b369e842b9530ab19e
Benni Mack <benni@typo3.org>
2018-10-24 12:06:05 +0200

    [TASK] Update to v2.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "memolist".
-#
-# Auto generated 06-10-2011 13:36
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Memolist: API Functions to attach data to a FE session or user record',
 	'description' => 'Memolist is a simple helper extension for everybody who needs to store data (like a memolist, a wishlist) that is attached to a website user (can be located in the users\' session or a logged-in user). Helpful for programmers only, as it is just a better abstraction to existing functions.',
@@ -22,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'priority' => '',
 	'state' => 'stable',
 	'clearCacheOnLoad' => 0,
-	'version' => '2.0.1',
+	'version' => '2.0.2',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.2.0-9.5.99',
