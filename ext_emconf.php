4f42b49651251b5338874ad68997dc9dfb53c68f
Benni Mack <benjamin.mack@b13.com>
2021-02-04 11:38:39 +0100

    [TASK] Update to 2.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,10 +12,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'priority' => '',
 	'state' => 'stable',
 	'clearCacheOnLoad' => 0,
-	'version' => '2.0.2',
+	'version' => '2.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-9.5.99',
+			'typo3' => '6.2.0-10.4.99',
 		),
 		'conflicts' => array(
 		),
